8502b55695730562259a2896bfe89d06fce9a8dd
kavitaparsewar <parsewarkavita@gmail.com>
2021-09-13 14:37:03 +0530

    [Start.cs]added successfully

---
+++ start.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day3Emp_PP
+{
+    class start
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+        }
+    }
+}
